1f03843ae5c42844c2dfd31511dfc9a4c5bd3a53
Andrew Bennett <andrew.bennett@medopad.com>
2018-07-27 09:18:42 +0100

    clean up and moved data class

--- app/src/main/java/avsoftware/com/fingertap/sensors/accelerometer/TapSensor.kt
+++ app/src/main/java/avsoftware/com/fingertap/sensors/accelerometer/TapSensor.kt
@@ -28,43 +28,22 @@ class TapSensor(){
         val currentTapTime: BehaviorSubject<Long> = BehaviorSubject.createDefault(0L)
 
         // Down Event Time is Storex in currentTapTime
-        val downEvents = motionEvents.filter { t: MotionEvent -> t.action == MotionEvent.ACTION_DOWN }
-                .doOnNext { t -> currentTapTime.onNext(SystemClock.currentThreadTimeMillis()) }
+        val downEvents = motionEvents.filter { it.action == MotionEvent.ACTION_DOWN }
+                .doOnNext { currentTapTime.onNext(SystemClock.currentThreadTimeMillis()) }
                 .ignoreElements()
 
         // Up Event is mapped to TapData instance - downTime property taken from currentTapTime
-        val upEvents = motionEvents.filter { t: MotionEvent -> t.action == MotionEvent.ACTION_UP }
+        val upEvents = motionEvents.filter { it.action == MotionEvent.ACTION_UP }
                 .withLatestFrom(currentTapTime, BiFunction { motion: MotionEvent, downTime: Long -> Pair(motion, downTime)  })
-                .map { pair: Pair<MotionEvent, Long> -> TapData(buttonId, pair.second.toFloat(), (SystemClock.currentThreadTimeMillis() - pair.second).toFloat(), pair.first.rawX, pair.first.rawY) }
+                // construct TapData event from pair of Up event and Down Time
+                .map { TapData(buttonId, it.second.toFloat(), (SystemClock.currentThreadTimeMillis() - it.second).toFloat(), it.first.rawX, it.first.rawY) }
 
 
         return Observable.merge(downEvents.toObservable(), upEvents)
     }
+}
 
-    // CLASS to store tap data
-    data class TapData(var iButton: Int, var fStartTime: Float, var fDuration: Float, var x: Float, var y: Float) : Recordable {
-        override fun getRecordableString(): String { return "\"<ORKTappingSample: 0x00000000; button: $iButton; timestamp: $fStartTime; timestamp: $fDuration; location: {$x, $y}>\"" }
-    }
-
-//    fun mapMotionEventToTapData( event: MotionEvent): SensorTap.TapData {
-//
-//        var index = event.actionIndex
-//        val action = event.actionMasked
-//        val pointerId = event.getPointerId(index)
-//
-//        if (SensorTap.iTimer == 0L) { SensorTap.iTimer = System.currentTimeMillis() }                               // start timer
-//
-//        when (action) {
-//            MotionEvent.ACTION_POINTER_DOWN, MotionEvent.ACTION_DOWN -> if (iFingerCount == 0) {
-//                touchTime = timer
-//                fingerId = pointerId
-//                iFingerCount++
-//            }
-//
-//            MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_CANCEL -> if (iFingerCount > 0 && fingerId == pointerId) {
-//                iFingerCount--
-//                behObs.onNext(SensorTap.TapData(iButton, touchTime, timer - touchTime, event.rawX, event.rawY))
-//            }
-//        }
-//    }
+// CLASS to store tap data
+data class TapData(var iButton: Int, var fStartTime: Float, var fDuration: Float, var x: Float, var y: Float) : Recordable {
+    override fun getRecordableString(): String { return "\"<ORKTappingSample: 0x00000000; button: $iButton; timestamp: $fStartTime; timestamp: $fDuration; location: {$x, $y}>\"" }
 }
